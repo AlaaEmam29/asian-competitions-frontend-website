68abf452937f80439da99e521b19f784efbea1e0
Alaa Emam <alaa.shaban.527@gmail.com>
2024-10-08 12:53:36 +0300

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL || 'https://cors-proxy.fringe.zone/https://api.sportmonks.com/v3/';
+export const BASE_URL = import.meta.env.VITE_BASE_URL || 'https://api.sportmonks.com/v3/';
 
  export const API_TOKEN = import.meta.env.VITE_API_TOKEN || 'LWmZZ9GncgZM4FpJuBDTsFTXZXgpO7OtAnpEtd01TS2S3irlVjgXRYc6XZKq'
 
